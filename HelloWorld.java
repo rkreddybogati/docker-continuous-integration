34a1c215145eb2fa346ba17ba2e9a58d0d5e4c41
Ramakrishna Bhogati <Ramakrishna.Bhogati@ipsoft.com>
2016-06-03 15:49:22 +0530

    Docker Automate Build number 1.

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,8 @@ public class HelloWorld {
 		System.out.println(" Docker In Continous Integration. ");
  
 		System.out.println("Hello World. ");
+
+		System.out.println("Docker Automate Build number 1. ");
 	}
 }
 
